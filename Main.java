c53ab343ed575c11c415c16941b6558a01d92c99
Feng Zhou <feng.zhou.0x@gmail.com>
2025-05-15 01:53:16 +1000

    Delete bluej file and make the Main class the entry point

--- Main.java
+++ Main.java
@@ -2,32 +2,17 @@
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
     /**
      * Constructor for objects of class Main
      */
-    public Main()
+    public static void main(String[] args)
     {
         // initialise instance variables
-        x = 0;
+        System.out.println("Hello World!");
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
